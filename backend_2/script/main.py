ec92ea64994c2fd2bbd09472ac98ce88a6ec76ab
albertoZurini <alberto.zurinI@gmail.com>
2021-04-09 22:54:08 +0200

    added chatbot

--- backend_2/script/main.py
+++ backend_2/script/main.py
@@ -8,6 +8,7 @@ import json
 from utils import image
 import matplotlib
 import matplotlib.pyplot as plt
+
 matplotlib.use('TkAgg')
 import time
 from neuralNet import predict as nnPredict
@@ -15,17 +16,16 @@ from neuralNet import predict as nnPredict
 app = Flask(__name__, static_url_path='')
 CORS(app)
 
-
 devices = [
-    {"name": "mouse",
-     "url": "https://www.st.com/content/ccc/fragment/application_related/end_app_information/end_app_block_diagram/group1/29/c3/4a/0e/6d/b0/44/aa/peripherals_mouse_image/files/peripherals_mouse.jpg/_jcr_content/translations/en.peripherals_mouse.jpg"},
-    {"name": "laptop",
-     "url": "https://www.st.com/content/ccc/fragment/application_related/end_app_information/end_app_block_diagram/group1/72/cd/d6/b5/9a/a8/4e/63/computers_peripherals_laptop_image/files/computers_peripherals_laptop.jpg/_jcr_content/translations/en.computers_peripherals_laptop.jpg"},
-    {"name": "notebook",
-    "url": "https://www.st.com/content/ccc/fragment/application_related/end_app_information/end_app_block_diagram/group1/72/cd/d6/b5/9a/a8/4e/63/computers_peripherals_laptop_image/files/computers_peripherals_laptop.jpg/_jcr_content/translations/en.computers_peripherals_laptop.jpg"},
+    {"class": "mouse",
+     "imgEndpoint": "https://www.st.com/content/ccc/fragment/application_related/end_app_information/end_app_block_diagram/group1/29/c3/4a/0e/6d/b0/44/aa/peripherals_mouse_image/files/peripherals_mouse.jpg/_jcr_content/translations/en.peripherals_mouse.jpg"},
+    {"class": "laptop",
+     "imgEndpoint": "https://www.st.com/content/ccc/fragment/application_related/end_app_information/end_app_block_diagram/group1/72/cd/d6/b5/9a/a8/4e/63/computers_peripherals_laptop_image/files/computers_peripherals_laptop.jpg/_jcr_content/translations/en.computers_peripherals_laptop.jpg"},
+    {"class": "notebook",
+     "imgEndpoint": "https://www.st.com/content/ccc/fragment/application_related/end_app_information/end_app_block_diagram/group1/72/cd/d6/b5/9a/a8/4e/63/computers_peripherals_laptop_image/files/computers_peripherals_laptop.jpg/_jcr_content/translations/en.computers_peripherals_laptop.jpg"},
     {
-        "name": "desktop_computer",
-        "url": "https://www.st.com/content/ccc/fragment/application_related/end_app_information/end_app_block_diagram/group1/8b/3c/e1/eb/05/9d/4f/90/computers_peripherals_desktop_image/files/computers_peripherals_desktop.jpg/_jcr_content/translations/en.computers_peripherals_desktop.jpg"
+    "class": "desktop_computer",
+    "imgEndpoint": "https://www.st.com/content/ccc/fragment/application_related/end_app_information/end_app_block_diagram/group1/8b/3c/e1/eb/05/9d/4f/90/computers_peripherals_desktop_image/files/computers_peripherals_desktop.jpg/_jcr_content/translations/en.computers_peripherals_desktop.jpg"
     }
 ]
 
@@ -45,8 +45,8 @@ def predict():
         for pred in res:
             url = "none"
             for device in devices:
-                if device["name"] == pred[1]:
-                    url = device["url"]
+                if device["class"] == pred[1]:
+                    url = device["imgEndpoint"]
                     break
 
             ret.append({
@@ -63,20 +63,13 @@ def predict():
 
 @app.route('/v1/classify/classifyText', methods=['POST'])
 def retFile():
-    ret = [
-        {
-            "found": True,
-            "class": "Mouse",
-            "imgEndpoint": "https://www.st.com/content/ccc/fragment/application_related/end_app_information/end_app_block_diagram/group0/2e/a5/77/2f/ba/2c/41/ea/mobile_pos_image/files/mobile_pos.jpg/_jcr_content/translations/en.mobile_pos.jpg",
-            "score": 0.96
-        },
-        {
-            "found": True,
-            "class": "Mouse",
-            "imgEndpoint": "https://www.st.com/content/ccc/fragment/application_related/end_app_information/end_app_block_diagram/group0/2e/a5/77/2f/ba/2c/41/ea/mobile_pos_image/files/mobile_pos.jpg/_jcr_content/translations/en.mobile_pos.jpg",
-            "score": 0.95
-        }
-    ]
+    query = request.json['query']
+
+    ret = []
+    for device in devices:
+        if device["class"] in query:
+            ret.append(device)
+
     resp = Response(response=json.dumps(ret),
                     status=200,
                     mimetype="application/json")
@@ -86,7 +79,7 @@ def retFile():
 @app.route("/v1/classify/getInfo/<path:path>", methods=['GET'])
 def getInfo(path):
     print(path)
-    ret =[
+    ret = [
         {
             "componentName": "BALF-NRG-02D3",
             "description": "Programmable Bluetooth&reg; LE 5.2 Wireless SoC"
