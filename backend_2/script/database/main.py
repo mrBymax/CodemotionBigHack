cfe24ce1b043bbd223ca90c88e9ebf496328f10e
Federico Bertossi <livebymax@gmail.com>
2021-04-09 22:57:18 +0200

    get_component_

---
+++ backend_2/script/database/main.py
@@ -0,0 +1,48 @@
+"""
+es.
+input: mouse
+output: tutte le componenti che fanno un mouse
+
+return: 
+
+[
+    {
+        name: "bla bla"
+        description: bla bla bla
+    }
+]
+
+----> pandas
+"""
+
+import pandas as pd
+
+
+def get_component(name):
+
+    data = pd.read_csv('Sheet0-Table1.csv',
+                    error_bad_lines=False, sep=';', header=0)
+
+    df = pd.DataFrame(data, columns=[
+                    'CATEGORY_NAME', 'LEVEL_NO', 'PART_NUMBER', 'GENERAL_DESCRIPTION'])
+
+    df.set_index('LEVEL_NO')
+
+    df = df.loc[(df['LEVEL_NO'] == 4)]
+
+    new_list = df.values.tolist()
+
+    ret = []
+
+    for item in new_list:
+        if name in item[0]:
+            ret.append({
+                "name": item[2],
+                "description": item[3]
+            })
+
+    print(len(ret))
+
+    
+
+
